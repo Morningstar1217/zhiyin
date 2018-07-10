f17a7ee3817cc868cde6d4c83e20616fd6e25064
Morningstarzz <2702155238@qq.com>
2018-07-10 11:58:55 +0800

    修改部分样式

--- wechat-app-mall-master/pages/address-add/index.js
+++ wechat-app-mall-master/pages/address-add/index.js
@@ -3,78 +3,78 @@ var commonCityData = require('../../utils/city.js')
 var app = getApp()
 Page({
   data: {
-    provinces:[],
-    citys:[],
-    districts:[],
-    selProvince:'请选择',
-    selCity:'请选择',
-    selDistrict:'请选择',
-    selProvinceIndex:0,
-    selCityIndex:0,
-    selDistrictIndex:0
+    provinces: [],
+    citys: [],
+    districts: [],
+    selProvince: '请选择',
+    selCity: '请选择',
+    selDistrict: '请选择',
+    selProvinceIndex: 0,
+    selCityIndex: 0,
+    selDistrictIndex: 0
   },
-  bindCancel:function () {
+  bindCancel: function () {
     wx.navigateBack({})
   },
-  bindSave: function(e) {
+  bindSave: function (e) {
     var that = this;
     var linkMan = e.detail.value.linkMan;
     var address = e.detail.value.address;
     var mobile = e.detail.value.mobile;
     var code = e.detail.value.code;
 
-    if (linkMan == ""){
+    if (linkMan == "") {
       wx.showModal({
         title: '提示',
         content: '请填写联系人姓名',
-        showCancel:false
+        showCancel: false
       })
       return
     }
-    if (mobile == ""){
+    if (mobile == "") {
       wx.showModal({
         title: '提示',
         content: '请填写手机号码',
-        showCancel:false
+        showCancel: false
       })
       return
     }
-    if (this.data.selProvince == "请选择"){
+    if (this.data.selProvince == "请选择") {
       wx.showModal({
         title: '提示',
         content: '请选择地区',
-        showCancel:false
+        showCancel: false
       })
       return
     }
-    if (this.data.selCity == "请选择"){
+    if (this.data.selCity == "请选择") {
       wx.showModal({
         title: '提示',
         content: '请选择地区',
-        showCancel:false
+        showCancel: false
       })
       return
     }
     var cityId = commonCityData.cityData[this.data.selProvinceIndex].cityList[this.data.selCityIndex].id;
     var districtId;
-    if (this.data.selDistrict == "请选择" || !this.data.selDistrict){
+    if (this.data.selDistrict == "请选择" || !this.data.selDistrict) {
       districtId = '';
     } else {
       districtId = commonCityData.cityData[this.data.selProvinceIndex].cityList[this.data.selCityIndex].districtList[this.data.selDistrictIndex].id;
     }
-    if (address == ""){
+    if (address == "") {
       wx.showModal({
         title: '提示',
         content: '请填写详细地址',
-        showCancel:false
+        showCancel: false
       })
       return
     }
-    if (code == ""){
+    if (code == "") {
       wx.showModal({
         title: '提示',
         content: '请填写邮编',
-        showCancel:false
+        showCancel: false
       })
       return
     }
@@ -93,20 +93,21 @@ Page({
         provinceId: commonCityData.cityData[this.data.selProvinceIndex].id,
         cityId: cityId,
         districtId: districtId,
-        linkMan:linkMan,
-        address:address,
-        mobile:mobile,
-        code:code,
-        isDefault:'true'
+        linkMan: linkMan,
+        address: address,
+        mobile: mobile,
+        code: code,
+        isDefault: 'true'
       },
-      success: function(res) {
+      success: function (res) {
+        console.log(res)
         if (res.data.code != 0) {
           // 登录错误 
           wx.hideLoading();
           wx.showModal({
             title: '失败',
             content: res.data.msg,
-            showCancel:false
+            showCancel: false
           })
           return;
         }
@@ -115,59 +116,59 @@ Page({
       }
     })
   },
-  initCityData:function(level, obj){
-    if(level == 1){
+  initCityData: function (level, obj) {
+    if (level == 1) {
       var pinkArray = [];
-      for(var i = 0;i<commonCityData.cityData.length;i++){
+      for (var i = 0; i < commonCityData.cityData.length; i++) {
         pinkArray.push(commonCityData.cityData[i].name);
       }
       this.setData({
-        provinces:pinkArray
+        provinces: pinkArray
       });
-    } else if (level == 2){
+    } else if (level == 2) {
       var pinkArray = [];
       var dataArray = obj.cityList
-      for(var i = 0;i<dataArray.length;i++){
+      for (var i = 0; i < dataArray.length; i++) {
         pinkArray.push(dataArray[i].name);
       }
       this.setData({
-        citys:pinkArray
+        citys: pinkArray
       });
-    } else if (level == 3){
+    } else if (level == 3) {
       var pinkArray = [];
       var dataArray = obj.districtList
-      for(var i = 0;i<dataArray.length;i++){
+      for (var i = 0; i < dataArray.length; i++) {
         pinkArray.push(dataArray[i].name);
       }
       this.setData({
-        districts:pinkArray
+        districts: pinkArray
       });
     }
-    
+
   },
-  bindPickerProvinceChange:function(event){
+  bindPickerProvinceChange: function (event) {
     var selIterm = commonCityData.cityData[event.detail.value];
     this.setData({
-      selProvince:selIterm.name,
-      selProvinceIndex:event.detail.value,
-      selCity:'请选择',
-      selCityIndex:0,
-      selDistrict:'请选择',
+      selProvince: selIterm.name,
+      selProvinceIndex: event.detail.value,
+      selCity: '请选择',
+      selCityIndex: 0,
+      selDistrict: '请选择',
       selDistrictIndex: 0
     })
     this.initCityData(2, selIterm)
   },
-  bindPickerCityChange:function (event) {
+  bindPickerCityChange: function (event) {
     var selIterm = commonCityData.cityData[this.data.selProvinceIndex].cityList[event.detail.value];
     this.setData({
-      selCity:selIterm.name,
-      selCityIndex:event.detail.value,
+      selCity: selIterm.name,
+      selCityIndex: event.detail.value,
       selDistrict: '请选择',
       selDistrictIndex: 0
     })
     this.initCityData(3, selIterm)
   },
-  bindPickerChange:function (event) {
+  bindPickerChange: function (event) {
     var selIterm = commonCityData.cityData[this.data.selProvinceIndex].cityList[this.data.selCityIndex].districtList[event.detail.value];
     if (selIterm && selIterm.name && event.detail.value) {
       this.setData({
@@ -193,12 +194,12 @@ Page({
           wx.hideLoading();
           if (res.data.code == 0) {
             that.setData({
-              id:id,
+              id: id,
               addressData: res.data.data,
               selProvince: res.data.data.provinceStr,
               selCity: res.data.data.cityStr,
               selDistrict: res.data.data.areaStr
-              });
+            });
             that.setDBSaveAddressId(res.data.data);
             return;
           } else {
@@ -212,7 +213,7 @@ Page({
       })
     }
   },
-  setDBSaveAddressId: function(data) {
+  setDBSaveAddressId: function (data) {
     var retSelIdx = 0;
     for (var i = 0; i < commonCityData.cityData.length; i++) {
       if (data.provinceId == commonCityData.cityData[i].id) {
@@ -229,9 +230,9 @@ Page({
         }
       }
     }
-   },
+  },
   selectCity: function () {
-    
+
   },
   deleteAddress: function (e) {
     var that = this;
@@ -257,7 +258,7 @@ Page({
       }
     })
   },
-  readFromWx : function () {
+  readFromWx: function () {
     let that = this;
     wx.chooseAddress({
       success: function (res) {
@@ -268,24 +269,36 @@ Page({
 
         for (var i = 0; i < commonCityData.cityData.length; i++) {
           if (provinceName == commonCityData.cityData[i].name) {
-            let eventJ = { detail: { value:i }};
+            let eventJ = {
+              detail: {
+                value: i
+              }
+            };
             that.bindPickerProvinceChange(eventJ);
             that.data.selProvinceIndex = i;
             for (var j = 0; j < commonCityData.cityData[i].cityList.length; j++) {
               if (cityName == commonCityData.cityData[i].cityList[j].name) {
                 //that.data.selCityIndex = j;
-                eventJ = { detail: { value: j } };
+                eventJ = {
+                  detail: {
+                    value: j
+                  }
+                };
                 that.bindPickerCityChange(eventJ);
                 for (var k = 0; k < commonCityData.cityData[i].cityList[j].districtList.length; k++) {
                   if (diatrictName == commonCityData.cityData[i].cityList[j].districtList[k].name) {
                     //that.data.selDistrictIndex = k;
-                    eventJ = { detail: { value: k } };
+                    eventJ = {
+                      detail: {
+                        value: k
+                      }
+                    };
                     that.bindPickerChange(eventJ);
                   }
                 }
               }
             }
-            
+
           }
         }
 
